06ef1988a02d38b0176b84e5cfebfa76c7f05fae
ilanasm <111690292+ilanasm@users.noreply.github.com>
2024-11-16 17:45:37 +0200

    Update main.tf

--- ilanastf/main.tf
+++ ilanastf/main.tf
@@ -1,15 +1,26 @@
+provider "aws" {
+  region = us-east-1
+}
+
+# ECS Cluster
 resource "aws_ecs_cluster" "flask_cluster" {
   name = "flask-api-cluster"
 }
 
+# ECR Repository
 resource "aws_ecr_repository" "flask_api_repo" {
   name = "ilanas-flask-api"
 }
 
+# Application Load Balancer (ALB)
 resource "aws_lb" "flask_lb" {
-  name = "flask-api-alb"
+  name               = "flask-api-alb"
+  load_balancer_type = "application"
+  subnets            = var.public_subnets
+  security_groups    = [aws_security_group.alb_sg.id]
 }
 
+# CloudFront Distribution
 resource "aws_cloudfront_distribution" "flask_distribution" {
   origin {
     domain_name = aws_lb.flask_lb.dns_name
@@ -24,4 +35,3 @@ resource "aws_cloudfront_distribution" "flask_distribution" {
 
   enabled = true
 }
-
