4ceb5ddad67a579f2753199267c5db30fba34478
ilanasm <111690292+ilanasm@users.noreply.github.com>
2024-11-16 18:00:45 +0200

    Update main.tf

--- ilanastf/main.tf
+++ ilanastf/main.tf
@@ -22,6 +22,10 @@ resource "aws_lb" "flask_lb" {
 
 # CloudFront Distribution
 resource "aws_cloudfront_distribution" "flask_distribution" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  # Origins
   origin {
     domain_name = aws_lb.flask_lb.dns_name
     origin_id   = "flask-api-origin"
@@ -30,8 +34,68 @@ resource "aws_cloudfront_distribution" "flask_distribution" {
       http_port              = 80
       https_port             = 443
       origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"] # Required argument
     }
   }
 
-  enabled = true
+  origin {
+    domain_name = aws_s3_bucket.flask_index.bucket_regional_domain_name
+    origin_id   = "flask-s3-origin"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.s3_identity.cloudfront_access_identity_path
+    }
+  }
+
+  # Default Cache Behavior
+  default_cache_behavior {
+    target_origin_id       = "flask-s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  # Cache Behavior for /v1/*
+  cache_behavior {
+    path_pattern           = "/v1/*"
+    target_origin_id       = "flask-api-origin"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  # Viewer Certificate
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  # Restrictions
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  # Logging (Optional)
+  logging_config {
+    bucket = "${aws_s3_bucket.logs.bucket_domain_name}"
+    prefix = "cloudfront/"
+    include_cookies = false
+  }
 }
