56539a081fd7b481fd0c11d1c634245fa2f585ed
ilanasm <111690292+ilanasm@users.noreply.github.com>
2024-11-18 11:32:55 +0200

    Update main.tf

--- ilanastf/main.tf
+++ ilanastf/main.tf
@@ -49,6 +49,8 @@ resource "aws_lb_listener" "http_listener" {
 }
 
 # IAM Roles and Policies for ECS
+
+# Execution Role
 resource "aws_iam_role" "ecs_execution_role" {
   name = "ecsExecutionRole"
 
@@ -87,6 +89,22 @@ resource "aws_cloudfront_distribution" "flask_distribution" {
       https_port             = 443
       origin_protocol_policy = "http-only"
       origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  # Default Cache Behavior
+  default_cache_behavior {
+    target_origin_id       = "flask-api-origin"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
     }
   }
 
