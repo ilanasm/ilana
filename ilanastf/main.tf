0bcde7bbea75bca5b57a09a697b23524e9f23e8d
ilanasm <111690292+ilanasm@users.noreply.github.com>
2024-11-18 09:12:38 +0200

    Update main.tf

--- ilanastf/main.tf
+++ ilanastf/main.tf
@@ -48,6 +48,50 @@ resource "aws_lb_listener" "http_listener" {
   }
 }
 
+# IAM Roles and Policies for ECS
+
+# Execution Role
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecsExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "ecs_execution_policy" {
+  name       = "ecsExecutionPolicyAttachment"
+  roles      = [aws_iam_role.ecs_execution_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# Task Role
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecsTaskRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
 # ECS Task Definition
 resource "aws_ecs_task_definition" "flask_task" {
   family                   = "flask-api-task"
